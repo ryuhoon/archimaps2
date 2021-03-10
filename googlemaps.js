3e995ebd1918cd41ddcd7e23f8c4da281757da93
H Ryu <archiryuhoon@gmail.com>
2021-03-10 13:54:32 +0000

    0310

--- googlemaps.js
+++ googlemaps.js
@@ -190,23 +190,86 @@ stylers: [
   }
 ],
             {name: 'Styled Map'});
-
-
+	
+		
+		
 							const map =
 							new google.maps.Map(document.getElementById('map'),
 							{
 								zoom: 12,
 								mapTypeControl: false,
-								center:
+								center: 
 								{ lat: 35.465746, lng: 139.622092}
+								
 							} );
+		const tokyomap = {lat : 35.658450658166664, lng:139.7007586504356}
+		const yokohamamap = {lat: 35.465746, lng: 139.622092}
 
 //Associate the styled map with the MapTypeId and set it to display.
         map.mapTypes.set('styled_map', styledMapType);
         map.setMapTypeId('styled_map');
-							
-							
+						
+		
+function CenterControl(controlDiv, map) {
+  // Set CSS for the control border.
+  const controlUI = document.createElement("div");
+  controlUI.style.backgroundColor = "#fff";
+  controlUI.style.border = "2px solid #fff";
+  controlUI.style.borderRadius = "3px";
+  controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+  controlUI.style.cursor = "pointer";
+  controlUI.style.marginBottom = "22px";
+  controlUI.style.textAlign = "center";
+  controlUI.title = "Click to recenter the Tokyo map";
+  controlDiv.appendChild(controlUI);
+  // Set CSS for the control interior.
+  const controlText = document.createElement("div");
+  controlText.style.color = "rgb(25,25,25)";
+  controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+  controlText.style.fontSize = "16px";
+  controlText.style.lineHeight = "38px";
+  controlText.style.paddingLeft = "5px";
+  controlText.style.paddingRight = "5px";
+  controlText.innerHTML = "Tokyo";
+  controlUI.appendChild(controlText);
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener("click", () => {
+    map.setCenter(tokyomap);
+	  map.setZoom(12);
+  });
+}	
+		
+function yokohamamapControl(yokohamamapcontrolDiv, map) {
+  // Set CSS for the control border.
+  const controlUI = document.createElement("div");
+  controlUI.style.backgroundColor = "#fff";
+  controlUI.style.border = "2px solid #fff";
+  controlUI.style.borderRadius = "3px";
+  controlUI.style.boxShadow = "0 2px 6px rgba(0,0,0,.3)";
+  controlUI.style.cursor = "pointer";
+  controlUI.style.marginBottom = "22px";
+  controlUI.style.textAlign = "center";
+  controlUI.title = "Click to recenter the Yokohama map";
+  yokohamamapcontrolDiv.appendChild(controlUI);
+  // Set CSS for the control interior.
+  const controlText = document.createElement("div");
+  controlText.style.color = "rgb(25,25,25)";
+  controlText.style.fontFamily = "Roboto,Arial,sans-serif";
+  controlText.style.fontSize = "16px";
+  controlText.style.lineHeight = "38px";
+  controlText.style.paddingLeft = "5px";
+  controlText.style.paddingRight = "5px";
+  controlText.innerHTML = "Yokohama";
+  controlUI.appendChild(controlText);
+  // Setup the click event listeners: simply set the map to Chicago.
+  controlUI.addEventListener("click", () => {
+    map.setCenter(yokohamamap);
+	   map.setZoom(12);
+  });
+}				
+	
 
+		
 	var infowindow = new google.maps.InfoWindow();
     var marker, i;
 		
@@ -252,12 +315,26 @@ stylers: [
       if(marker)
       {
         marker.addListener('click', function() {
-          map.setZoom(14);
+          map.setZoom(15);
           map.setCenter(this.getPosition());
         });
         }
     }
+	
 		
+	// Create the DIV to hold the control and call the CenterControl()
+  // constructor passing in this DIV.
+  const centerControlDiv = document.createElement("div");
+  CenterControl(centerControlDiv, map);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(centerControlDiv);
 		
+// Create the DIV to hold the control and call the CenterControl()
+  // constructor passing in this DIV.
+  const yokohamamapControlDiv = document.createElement("div");
+  yokohamamapControl(centerControlDiv, map);
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(yokohamamapControlDiv);
+
+
+
 }
 			
