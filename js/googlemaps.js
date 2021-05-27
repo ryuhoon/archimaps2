1d83f00f43bb10ca0e200f499ae59f1c12186a30
ryuhoon <archiryuhoon@gmail.com>
2021-05-28 01:36:52 +0900

    fgfgf

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -296,9 +296,8 @@ function yokohamamapControl(yokohamamapcontrolDiv, map) {
                 '<div id="siteNotice">' +
 								'</div>'
 
-                 + '<div style="width:130px;float:left">'+'<img src="'+ndata[i]['img']+'"  width="130" />'+'</div>'        
-                 + '<div class="img-box">'+'<img src="https://image.shutterstock.com/image-vector/palace-icon-outline-vector-web-260nw-1046855677.jpg">'+'</div>'
-
+                 + '<div class="img-box">' + '<img src="'+ ndata[i]['img']+'"  width="130" />' +'</div>'
+                 
 								 +'<h1 id="firstHeading" class="firstHeading">' 
 								 + ndata[i]['name'] +'</h1>' 
                 
