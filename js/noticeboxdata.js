58d8290741c72e873d10d63121c65d5f07eead98
ryuhoon <archiryuhoon@gmail.com>
2021-07-09 17:40:04 +0900

    dsdsds

---
+++ js/noticeboxdata.js
@@ -0,0 +1,17 @@
+var noticeboxdata = [
+	{
+	  noticeday:2021/06/29,
+    noticetext:"맵리스트 추가완료 도쿄, 요코하마 지역 추가됨 Shibuya Station Sendagaya Station Public Restroom",
+  },
+  {
+	  noticeday:2021/06/29,
+    noticetext:"맵리스트 추가완료 도쿄2, 요코하마 지역 추가됨 Shibuya Station Sendagaya Station Public Restroom",
+  },
+  {
+	  noticeday:2021/06/29,
+    noticetext:"맵리스트 추가완료 도쿄3, 요코하마 지역 추가됨 Shibuya Station Sendagaya Station Public Restroom",
+  },
+	
+    ];
+
+ 
