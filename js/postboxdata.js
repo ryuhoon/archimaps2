175a42bdea087554d408720ae5644d207cca20ff
ryuhoon <archiryuhoon@gmail.com>
2021-07-11 21:34:03 +0900

    dsdsdsdaa

--- js/postboxdata.js
+++ js/postboxdata.js
@@ -4,7 +4,26 @@ var postboxdata = [
     posttitle:'Blue Bottle Coffee Minato Mirai Cafe',
     postlink:'post1.html'
   },
-  
+  {
+	  postday:'2021/07/10',
+    posttitle:'GUNDAM FACTORY YOKOHAMA',
+    postlink:'post2.html'
+  },
+  {
+	  postday:'2021/07/10',
+    posttitle:'MIYASHITA PARK / TAKENAKA CORPORATION, NIKKEN SEKKEI',
+    postlink:'post3.html'
+  },
+  {
+	  postday:'2021/07/10',
+    posttitle:'Blue Bottle Coffee Minato Mirai Cafe',
+    postlink:'post1.html'
+  },
+  {
+	  postday:'2021/07/10',
+    posttitle:'Blue Bottle Coffee Minato Mirai Cafe',
+    postlink:'post1.html'
+  },
     ];
 
  
