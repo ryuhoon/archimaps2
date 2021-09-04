cdae751d0f5d8e6102aa84de59af14dbeda0bff5
ryuhoon <archiryuhoon@gmail.com>
2021-09-04 17:24:16 +0900

    sdasdasdas

--- js/noticeboxdata.js
+++ js/noticeboxdata.js
@@ -1,5 +1,9 @@
 var noticeboxdata = [
   {
+	  noticeday:'2021/09/04',
+    noticetext:'Tokyo data update.',
+  },
+  {
 	  noticeday:'2021/07/12',
     noticetext:'Book info add.',
   },
@@ -19,6 +23,7 @@ var noticeboxdata = [
 	  noticeday:'2021/06/29',
     noticetext:'도쿄, 요코하마 카페 추가됨 <br>name:"Blue Bottle Coffee Minato Mirai Cafe" <br>도쿄 화장실 프로젝트 추가됨. TOKYO Toilet Project',
   },
+  
 	
     ];
 
